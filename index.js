79faf0227a778c6fd26831d368b254e0c1c07f30
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-16 20:42:20 +0700

    call another module

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 // const cetakNama = (nama) => `Hi, nama saya ${nama}`;
 // console.log(cetakNama('ahdan firdaus'))
 
-const cetakNama = require('./coba')
+// const fs = require('fs'); // core module
+// const cetakNama = require('./coba') // local module
+// const moment = require('moment'); // third party module / npm module / node_modules
 
 // console.log('hello dadan')
 console.log(cetakNama('dadan'));
