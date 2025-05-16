83caf6b17a6414fceb155270afdd4b2a90cf413c
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-16 22:12:20 +0700

    combine core module readline & fs then save data to json

--- 7-core-node-module/app.js
+++ 7-core-node-module/app.js
@@ -32,7 +32,16 @@ const rl = readline.createInterface({
 
 rl.question("Masukan nama anda : ", (nama) => {
   rl.question('Masukkan no HP anda : ', (noHP) => {
-    console.log(`Terimakasih ${nama}, sudah menginputkan ${noHP}`);
+    const contact = {nama, noHP}
+    const file = fs.readFileSync('data/contacts.json', 'utf-8');
+    const contacts = JSON.parse(file);
+
+    contacts.push(contact);
+
+    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+
+    console.log("Terimakasih sudah memsukkan data.");
+
     rl.close();
-  })
+  });
 });
