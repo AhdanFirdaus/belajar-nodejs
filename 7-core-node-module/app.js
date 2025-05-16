2782df1ea37d1cd0afe4252d3e14c9af35edafd7
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-16 21:24:21 +0700

    write string to file (asynchronous)

--- 7-core-node-module/app.js
+++ 7-core-node-module/app.js
@@ -2,8 +2,14 @@
 const fs = require('node:fs');
 
 // menuliskan string ke file (synchronous)
-try {
-  fs.writeFileSync('data/test.txt', 'hello world secara synchronous')
-} catch (e) {
-  console.log(e);
-}
+// try {
+  //   fs.writeFileSync('data/test.txt', 'hello world secara synchronous')
+  // } catch (e) {
+    //   console.log(e);
+    // }
+    
+    
+// menuliskan string ke file (asynchronous)
+fs.writeFile('data/test.txt', 'hello world secara Asynchronous', (e) => {
+  console.log(e)
+})
