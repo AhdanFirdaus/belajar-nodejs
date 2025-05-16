add53202ee8acca157de625f7861ea267438b66c
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-16 20:26:23 +0700

    running a simple node file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // const nama = 'ahdan firdaus';
 // const cetakNama = (nama) => `Hi, nama saya ${nama}`;
 // console.log(cetakNama('ahdan firdaus'))
+
+require('./coba')
+
 console.log('hello dadan')
