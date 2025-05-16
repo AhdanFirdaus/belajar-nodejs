d799d54276a8dbafe579f9bd8e652c634dc02e6d
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-16 21:52:54 +0700

    using the core module readline and two questions

--- 7-core-node-module/app.js
+++ 7-core-node-module/app.js
@@ -1,25 +1,38 @@
 // Core module
-const fs = require('node:fs');
+const fs = require("node:fs");
 
 // menuliskan string ke file (synchronous)
 // try {
-  //   fs.writeFileSync('data/test.txt', 'hello world secara synchronous')
-  // } catch (e) {
-    //   console.log(e);
-    // }
-    
-    
+//   fs.writeFileSync('data/test.txt', 'hello world secara synchronous')
+// } catch (e) {
+//   console.log(e);
+// }
+
 // menuliskan string ke file (asynchronous)
 // fs.writeFile('data/test.txt', 'hello world secara Asynchronous', (e) => {
 //   console.log(e)
 // })
 
-// membaca isi file (synchronous) 
+// membaca isi file (synchronous)
 // const data = fs.readFileSync('data/test.txt', 'utf-8');
 // console.log(data.toString());
 
-// membaca isi file (asynchronous) 
-fs.readFile('data/test.txt', 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data.toString());
-}); 
+// membaca isi file (asynchronous)
+// fs.readFile('data/test.txt', 'utf-8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data.toString());
+// });
+
+// Readline
+const readline = require("node:readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Masukan nama anda : ", (nama) => {
+  rl.question('Masukkan no HP anda : ', (noHP) => {
+    console.log(`Terimakasih ${nama}, sudah menginputkan ${noHP}`);
+    rl.close();
+  })
+});
