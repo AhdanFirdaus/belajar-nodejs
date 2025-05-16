b88d70017ee93b9ca1f365ac5bf21226702bdc92
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-16 20:46:30 +0700

    calling a regular variable

--- coba.js
+++ coba.js
@@ -2,4 +2,7 @@ function cetakNama(nama) {
   return `halo, nama saya ${nama}`;
 }
 
+const PI = 3.14;
+
 module.exports = cetakNama;
+module.exports = PI;
--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 // console.log(cetakNama('ahdan firdaus'))
 
 // const fs = require('fs'); // core module
-// const cetakNama = require('./coba') // local module
+// const cetakNama = require('./coba'); // local module
 // const moment = require('moment'); // third party module / npm module / node_modules
 
 // console.log('hello dadan')
-console.log(cetakNama('dadan'));
+
+// const cetakNama = require('./coba');
+const PI = require('./coba');
+console.log(PI);
