d125cb2decf850f5d943b5e34306ff414cdf6d3b
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-27 21:17:35 +0700

    9th meeting install package chalk

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,10 @@
-console.log('hello world!');
+const validator = require('validator');
+const chalk = require('chalk');
+// console.log(validator.isEmail('muhammadahdanf1@gmail.com'));
+// console.log(validator.isMobilePhone('08113213213211', 'id-ID'));
+// console.log(validator.isNumeric('34324324'));
+
+// console.log(chalk.black.bgCyan('hello world!'));
+const nama = 'Ahdan';
+const pesan = chalk`Lorem ipsum dolor {bgRed.black sit amet}, consectetur adipiscing elit {bgGreen.italic.black vehicula}. Nama saya ${nama}`
+console.log(chalk(pesan));
