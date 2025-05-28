a498635239e267f701ddcbbed76928238ed35fa2
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-28 20:46:28 +0700

    using modular concept

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,46 +1,10 @@
-const fs = require("node:fs");
-const readline = require("node:readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-// membuat folder data jika belum ada
-const dirPath = "./data";
-if (!fs.existsSync(dirPath)) {
-  fs.mkdirSync(dirPath);
-}
-
-// membuat file contacts.json jika belum ada
-const dataPath = "./data/contacts.json";
-if (!fs.existsSync(dataPath)) {
-  fs.writeFileSync(dataPath, "[]", "utf-8");
-}
-
-const tulisPertanyaan = (pertanyaan) => {
-  return new Promise((resolve, rejects) => {
-    rl.question(pertanyaan, (nama) => {
-      resolve(nama)
-    })
-  })
-}
-
+const {tulisPertanyaan, simpanContact} = require('./contacts')
 const main = async () => {
   const nama = await tulisPertanyaan('Masukkan nama anda : ');
   const email = await tulisPertanyaan('Masukkan email anda : ');
   const noHP = await tulisPertanyaan('Masukkan No HP anda : ');
-  const contact = { nama, email, noHP };
-  const file = fs.readFileSync("data/contacts.json", "utf-8");
-  const contacts = JSON.parse(file);
-
-  contacts.push(contact);
-
-  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
-
-  console.log("Terimakasih sudah memsukkan data.");
-
-  rl.close();
+  
+  simpanContact(nama, email, noHP)
 };
 
 main()
