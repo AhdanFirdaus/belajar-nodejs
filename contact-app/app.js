f8d756d703217fb09e8e65a96d4d3dbbc5075760
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-28 21:26:06 +0700

    install package yargs and simple test

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,10 +1,37 @@
-const {tulisPertanyaan, simpanContact} = require('./contacts')
-const main = async () => {
-  const nama = await tulisPertanyaan('Masukkan nama anda : ');
-  const email = await tulisPertanyaan('Masukkan email anda : ');
-  const noHP = await tulisPertanyaan('Masukkan No HP anda : ');
-  
-  simpanContact(nama, email, noHP)
-};
+const yargs = require('yargs')
+// mengambil argument dari command line
+// const command = process.argv[2]
+// if(command === 'add'){
 
-main()
+// } else if(command === 'remove'){
+
+// } else if(command === 'list'){
+
+// }
+
+console.log(yargs.argv)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const {tulisPertanyaan, simpanContact} = require('./contacts')
+// const main = async () => {
+//   const nama = await tulisPertanyaan('Masukkan nama anda : ');
+//   const email = await tulisPertanyaan('Masukkan email anda : ');
+//   const noHP = await tulisPertanyaan('Masukkan No HP anda : ');
+  
+//   simpanContact(nama, email, noHP)
+// };
+
+// main()
