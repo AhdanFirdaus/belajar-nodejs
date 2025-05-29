2e5caab4f2ba2a36f3b7bb79bb0230e069748a66
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-29 20:46:23 +0700

    add email validation with validator package

--- contact-app/contacts.js
+++ contact-app/contacts.js
@@ -1,35 +1,46 @@
-const fs = require('node:fs')
-const chalk = require('chalk')
+const fs = require("node:fs");
+const chalk = require("chalk");
+const validator = require("validator");
 
 // membuat folder data jika belum ada
-const dirPath = "./data"
+const dirPath = "./data";
 if (!fs.existsSync(dirPath)) {
-  fs.mkdirSync(dirPath)
+  fs.mkdirSync(dirPath);
 }
 
 // membuat file contacts.json jika belum ada
-const dataPath = "./data/contacts.json"
+const dataPath = "./data/contacts.json";
 if (!fs.existsSync(dataPath)) {
-  fs.writeFileSync(dataPath, "[]", "utf-8")
+  fs.writeFileSync(dataPath, "[]", "utf-8");
 }
 
 const simpanContact = (nama, email, noHP) => {
-  const contact = { nama, email, noHP }
-  const file = fs.readFileSync("data/contacts.json", "utf-8")
-  const contacts = JSON.parse(file)
+  const contact = { nama, email, noHP };
+  const file = fs.readFileSync("data/contacts.json", "utf-8");
+  const contacts = JSON.parse(file);
 
   // cek duplikat
-  const duplikat = contacts.find((contact) => contact.nama === nama)
-  if(duplikat) {
-    console.log(chalk.red.inverse.bold('Contact sudah terdaftar, gunakan nama lain!'))
-    return false
+  const duplikat = contacts.find((contact) => contact.nama === nama);
+  if (duplikat) {
+    console.log(
+      chalk.red.inverse.bold("Contact sudah terdaftar, gunakan nama lain!")
+    );
+    return false;
   }
 
-  contacts.push(contact)
+  // cek email
+  if (email) {
+    if (!validator.isEmail(email)) {
+      console.log(chalk.red.inverse.bold("Email tidak valid!"));
+      return false;
+    }
+  }
 
-  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts))
+  contacts.push(contact);
 
-  console.log("Terimakasih sudah memsukkan data.")
-}
+  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
 
-module.exports = {simpanContact}
+  console.log("Terimakasih sudah memsukkan data.");
+};
+
+module.exports = { simpanContact };
