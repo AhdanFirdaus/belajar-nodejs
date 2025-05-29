fcc2f34da664476cc410d4be36a69036a679d918
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-29 22:45:27 +0700

    add simple routing

--- 13-modejs-web-server/app.js
+++ 13-modejs-web-server/app.js
@@ -1,10 +1,23 @@
 const http = require("http");
-const port = 3000
+const port = 3000;
 
 http
   .createServer((req, res) => {
-    res.write("Hello World!");
-    res.end();
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    const url = req.url;
+    if (url === "/about") {
+      res.write("<h1>Ini adalah halaman about</h1>");
+      res.end();
+    } else if (url === "/contact") {
+      res.write("<h1>Ini adalah halaman contact</h1>");
+      res.end();
+    } else {
+      res.write("<h1>Hello World!</h1>");
+      res.end();
+    }
   })
   .listen(port, () => {
     console.log(`Server listening on port ${port}..`);
