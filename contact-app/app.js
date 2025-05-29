b209c43ada4234c1bca977f3df29a96a89dee3aa
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-05-29 20:16:09 +0700

    manual yargs.command usage

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,37 +1,12 @@
-const yargs = require('yargs')
-// mengambil argument dari command line
-// const command = process.argv[2]
-// if(command === 'add'){
+const yargs = require("yargs");
 
-// } else if(command === 'remove'){
+yargs.command(
+  "add",
+  "Menambahkan contact baru",
+  () => {},
+  (argv) => {
+    console.log(argv.nama);
+  }
+);
 
-// } else if(command === 'list'){
-
-// }
-
-console.log(yargs.argv)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const {tulisPertanyaan, simpanContact} = require('./contacts')
-// const main = async () => {
-//   const nama = await tulisPertanyaan('Masukkan nama anda : ');
-//   const email = await tulisPertanyaan('Masukkan email anda : ');
-//   const noHP = await tulisPertanyaan('Masukkan No HP anda : ');
-  
-//   simpanContact(nama, email, noHP)
-// };
-
-// main()
+yargs.parse()
