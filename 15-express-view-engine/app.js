daa89db7b52697aa2c07a5be11cf849673746118
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-06-01 21:42:00 +0700

    install the express-ejs-layouts package and make it easier to manage the same code

--- 15-express-view-engine/app.js
+++ 15-express-view-engine/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const port = 3000;
 
 // gunakan ejs
 app.set("view engine", "ejs");
+app.use(expressLayouts);
 
 app.get("/", (req, res) => {
   const mahasiswa = [
@@ -20,19 +22,26 @@ app.get("/", (req, res) => {
       email: "rakuu@gmail.com",
     },
   ];
-  res.render("index", 
-    { nama: "Ahdan Firdaus", 
-      title: "Halaman Home",
-      mahasiswa
-    });
+  res.render("index", {
+    nama: "Ahdan Firdaus",
+    title: "Halaman Home",
+    mahasiswa,
+    layout: 'layouts/main-layout'
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", {title: 'Halaman About'});
+  res.render("about", {
+    layout: "layouts/main-layout",
+    title: "Halaman About",
+  });
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact", {title: 'Halaman Contact'});
+  res.render("contact", {
+    layout: "layouts/main-layout",
+    title: "Halaman Contact",
+  });
 });
 
 app.get("/product/:id", (req, res) => {
